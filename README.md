272426097738e439a38d2dded50d38ac3efe5c5c
SugoiDogo <josephsmendoza@gmail.com>
2023-01-24 05:42:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# DUETI
+
+Installer for DUET, a UEFI implementation for BIOS firmware
+
+## Requirements
+
+All you need is python and a set of DUET binaries. Both Clover and OpenCore include them in their release archives. The MBR file will always begin with `boot0` and the PBR file will always begin with `boot1`.
+
+## Usage
+
+### Darwin (Mac)
+
+```
+python3 dueti.py mbr boot0ss /dev/rdisk0
+python3 dueti.py pbr boot1f32alt /dev/rdisk0s1
+```
+
+### Linux
+
+```
+python3 dueti.py mbr boot0ss /dev/sda
+python3 dueti.py pbr boot1f32alt /dev/sda1
+```
+
+### NT (Windows)
+
+```
+python3 dueti.py mbr boot0ss //./PhysicalDisk1
+python3 dueti.py pbr boot1f32alt //./D:
+```
+
+
+
+## Overview
+
+This script installs DUET based on the [ArchWiki installation instructions for Clover on BIOS systems](https://wiki.archlinux.org/title/Clover#BIOS_Systems)
+
+```
+dd if=/dev/sda1 of=/tmp/original_PBR bs=512 count=1 conv=notrunc
+cp /mnt/iso/usr/standalone/i386/boot1f32 /tmp/new_PBR
+dd if=/tmp/original_PBR of=/tmp/new_PBR skip=3 seek=3 bs=1 count=87 conv=notrunc
+dd if=/tmp/new_PBR of=/dev/sda1 bs=512 count=1 conv=notrunc
+dd if=/mnt/iso/usr/standalone/i386/boot0ss of=/dev/sda bs=440 count=1 conv=notrunc
+```
+
+By analyzing these commands, and the contents of both Clover and OpenCore release archives, the installation process can be described as:
+
+1. Write first 440 bytes of `boot0*` to MBR
+2. Write first 3 bytes and last 422 bytes of `boot1*` to PBR
+3. Copy DUET bootloader to partition root as `boot`
+
+This script performs steps 1 and 2. Don't forgot to copy your bootloader over as well ( `boot6` or `boot7` for Clover, `bootx64` or `bootia32` for OpenCore ).
---
+++ dueti.py
@@ -0,0 +1,77 @@
+import os, argparse
+
+def writembr(drive,boot0):
+    drive=os.open(drive, os.O_RDWR | os.O_BINARY)
+    boot0=os.open(boot0, os.O_RDONLY | os.O_BINARY)
+
+    buffer=os.read(boot0, 440)
+    buffer+=os.read(drive, 512)[440:]
+    os.lseek(drive, 0, os.SEEK_SET)
+    os.write(drive, buffer)
+    os.fsync(drive)
+
+    os.close(boot0)
+    os.close(drive)
+
+def writepbr(part,boot1):
+    part=os.open(part, os.O_RDWR | os.O_BINARY)
+    boot1=os.open(boot1, os.O_RDONLY | os.O_BINARY)
+
+    buffer=os.read(boot1, 3)
+    buffer+=os.read(part, 512)[3:90]
+    os.lseek(boot1, 90, os.SEEK_SET)
+    buffer+=os.read(boot1, 422)
+    os.lseek(part, 0, os.SEEK_SET)
+    os.write(part, buffer)
+    os.fsync(part)
+
+    os.close(boot1)
+    os.close(part)
+
+parser=argparse.ArgumentParser(
+    prog='dueti',
+    description='DUET Installer'
+)
+subparsers=parser.add_subparsers(required=True)
+
+mbr=subparsers.add_parser(
+    'mbr',
+    description='Install MBR chainloader',
+    epilog='Examples of drive paths:\
+        Windows: //./PhysicalDrive0\
+        Linux: /dev/sda\
+        Darwin: /dev/rdisk0'
+)
+mbr.add_argument(
+    'boot0',
+    help='path to a DUET boot0 file, containing the MBR chainloader'
+)
+mbr.add_argument(
+    'drive',
+    help='path to destination device'
+)
+
+pbr=subparsers.add_parser(
+    'pbr',
+    description='Install PBR chainloader',
+    epilog='Examples of partition paths:\
+        Windows: //./C:\
+        Linux: /dev/sda1\
+        Darwin: /dev/rdisk0s1'
+)
+pbr.add_argument(
+    'boot1',
+    help='path to a DUET boot1 file, containing the PBR chainloader'
+)
+pbr.add_argument(
+    'partition',
+    help='path to destination partition'
+)
+
+args=parser.parse_args()
+
+if 'boot0' in args:
+    writembr(args.drive, args.boot0)
+
+if 'boot1' in args:
+    writepbr(args.partition, args.boot1)
